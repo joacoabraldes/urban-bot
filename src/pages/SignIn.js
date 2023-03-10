b826b40d5da70fb9d046c25cf1ff0a47e6f26c5c
joacoabraldes <abraldesjoaquin@gmail.com>
2023-03-10 17:58:34 -0300

    ....

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -33,7 +33,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
-const userRef = collection(db, "users", );
+const userRef = collection(db, "users");
 
 function SignIn() {
   const fetchId = async (mail, password) => {
@@ -42,13 +42,19 @@ function SignIn() {
       "path",
       "/Application/ec1d38d7-d359-48d0-a60c-d8c0b8fb9df9/Login?_c=es-ES"
     );
-    myHeaders.append("content-type", "application/json");
     myHeaders.append("authority", "services.mywellness.com");
+    myHeaders.append("content-type", "application/json");
+    myHeaders.append("x-mwapps-tz-olson", "America/Argentina/Buenos_Aires");
     myHeaders.append(
-      "Cookie",
-      "_mwappseu=ec1d38d7-d359-48d0-a60c-d8c0b8fb9df9|MjAyMzAzMTAxNDM1MDZ8NmRmNDJiNzM0NWIzNDA1Yjg5NWU0YjY2YzRlMGY3Y2J8ZWMxZDM4ZDdkMzU5NDhkMGE2MGNkOGMwYjhmYjlkZjl8NnxBcmdlbnRpbmEgU3RhbmRhcmQgVGltZXxlcy1FU3wxODU4YjY0ZDQwYmQ0N2VlODAyNTU0N2U2ODgzM2ZjYnx8fHwxfDF8MHwxMDB8fHw1OHwyMzg5fDB8Y29tLm15d2VsbG5lc3M1.0D3A2DD49E5E5CBE2F2DC9240F10DA45495C6FC28B4E5E3F714B96A8FEC89A863387B81F4B2EAA120544D8AEC283AA65EB30E8148586171A32D06E639B9F1700; _mwappseu=ec1d38d7-d359-48d0-a60c-d8c0b8fb9df9|MjAyMzAzMTAxOTEzNDh8ZmFhMDQyOTdlMDQxNDczZmE5ZWViODI5M2I5M2Y5YmV8ZWMxZDM4ZDdkMzU5NDhkMGE2MGNkOGMwYjhmYjlkZjl8NnxBcmdlbnRpbmEgU3RhbmRhcmQgVGltZXxlcy1FU3w5ZGQ2YjQwZTM3OWE0MjYwYTBlNTAwNGM1OTdhMTNhMHx8fHwxfDF8MHwxMDB8fHw1OHwzMzU0fDB8Y29tLm15d2VsbG5lc3M1.0A670B13F8A204047B92C40F8AED30E5A7377E514DDA38916D38A1D933CF6139D1DD658AE410D4DDE18252F433AC9909DFF26E5C503EFBFF68AF0777B91C6742"
+      "User-Agent",
+      "MywellnessCustom/12 CFNetwork/1404.0.5 Darwin/22.3.0"
     );
     myHeaders.append("x-mwapps-client", "mywellnessappios40");
+    myHeaders.append("x-mwapps-appid", "ec1d38d7-d359-48d0-a60c-d8c0b8fb9df9");
+    myHeaders.append(
+      "Cookie",
+      "_mwappseu=ec1d38d7-d359-48d0-a60c-d8c0b8fb9df9|MjAyMzAzMTAxNDM1MDZ8NmRmNDJiNzM0NWIzNDA1Yjg5NWU0YjY2YzRlMGY3Y2J8ZWMxZDM4ZDdkMzU5NDhkMGE2MGNkOGMwYjhmYjlkZjl8NnxBcmdlbnRpbmEgU3RhbmRhcmQgVGltZXxlcy1FU3wxODU4YjY0ZDQwYmQ0N2VlODAyNTU0N2U2ODgzM2ZjYnx8fHwxfDF8MHwxMDB8fHw1OHwyMzg5fDB8Y29tLm15d2VsbG5lc3M1.0D3A2DD49E5E5CBE2F2DC9240F10DA45495C6FC28B4E5E3F714B96A8FEC89A863387B81F4B2EAA120544D8AEC283AA65EB30E8148586171A32D06E639B9F1700; _mwappseu=ec1d38d7-d359-48d0-a60c-d8c0b8fb9df9|MjAyMzAzMTAyMDI3MjB8NmRmNDJiNzM0NWIzNDA1Yjg5NWU0YjY2YzRlMGY3Y2J8ZWMxZDM4ZDdkMzU5NDhkMGE2MGNkOGMwYjhmYjlkZjl8NnxBcmdlbnRpbmEgU3RhbmRhcmQgVGltZXxlcy1FU3wxODU4YjY0ZDQwYmQ0N2VlODAyNTU0N2U2ODgzM2ZjYnx8fHwxfDF8MHwxMDB8fHw1OHw3MDQ1fDB8Y29tLm15d2VsbG5lc3M1.CD29C8CF3CB66A95CD381DB718DDF1EAD198D53527BFCE393C3CCCC41E4613B2FAF50C3BD54A4B109A67016F25301F450B7A0791B66FCB36F757B112B86D4519"
+    );
     myHeaders.append(
       "Authorization",
       "Bearer MjAyMzAzMTAxNDM2MDB8NmRmNDJiNzM0NWIzNDA1Yjg5NWU0YjY2YzRlMGY3Y2J8ZWMxZDM4ZDdkMzU5NDhkMGE2MGNkOGMwYjhmYjlkZjl8NnxBcmdlbnRpbmEgU3RhbmRhcmQgVGltZXxlcy1FU3wxODU4YjY0ZDQwYmQ0N2VlODAyNTU0N2U2ODgzM2ZjYnx8fHwxfDF8MHwxMDB8fHw1OHw3ODE0fDB8Y29tLm15d2VsbG5lc3M1.02D84E07DBCAAFDB3E1394801EFD0D60C1F3EC00C75D9791D1AC798B7E87884307958104CEB2638045B6ADBBEAF0CC0A62B2FBBEF3FF4DD162C8576078016B13"
@@ -68,7 +74,7 @@ function SignIn() {
     };
 
     fetch(
-      "/api/https://services.mywellness.com/Application/ec1d38d7-d359-48d0-a60c-d8c0b8fb9df9/Login?_c=es-ES",
+      "https://services.mywellness.com/Application/ec1d38d7-d359-48d0-a60c-d8c0b8fb9df9/Login?_c=es-ES",
       requestOptions
     )
       .then((response) => response.text())
@@ -99,20 +105,24 @@ function SignIn() {
       setError(true);
     }
 
-    const id = fetchId(mail, password);
+    console.log(mail, password);
+
+    const id = await fetchId(mail, password);
+
+    console.log(id);
 
     const newUser = {
-        email: mail,
-        id: id,
-        Mon: [],
-        Tue: [],
-        Wed: [],
-        Thu: [],
-        Fri: [],
-        Sat: [],
-      };
-      
-      await setDoc(doc(userRef, mail), newUser);
+      email: mail,
+      id: id,
+      Mon: [],
+      Tue: [],
+      Wed: [],
+      Thu: [],
+      Fri: [],
+      Sat: [],
+    };
+
+    await setDoc(doc(userRef, mail), newUser);
   };
 
   return (
